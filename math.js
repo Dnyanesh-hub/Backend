26cb77fc00930db669945c8e5db6158f199d3f99
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-01-08 12:51:31 +0530

    started node.js learned about module.export how to export from one file to other and one directory to another directory and basic commands of node

---
+++ math.js
@@ -0,0 +1,12 @@
+const sum=(a,b)=>a+b;
+const multiply=(a,b)=>a*b;
+const g=9.8;
+const pi=3.14;
+
+
+module.exports={
+    sum:sum,
+    multiply:multiply,
+    g:g,
+    pi:pi,
+}
---
+++ script.js
@@ -0,0 +1,17 @@
+// console.log("hello backend");
+// let n=6;
+// for(let i=0;i<n;i++){
+//     console.log("hello",i);
+// }
+
+// const arg=process.argv;
+// for(let i=2;i<arg.length;i++){
+//     console.log("hello and welcome to dany empire:",arg[i]);
+// }
+// console.log("No mercy");
+const fruit=require("./fruits");
+console.log(fruit)
+// console.log(math.sum(2,2));
+// console.log(math.pi);
+// console.log(math.multiply(7,2));
+// console.log(math.g);

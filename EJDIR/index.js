d6d72eb5b8f2738d3c4adcd7e6feb27d95a3aaef
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-01-09 20:36:30 +0530

    Ejs control flow interpolation syntax

---
+++ EJDIR/index.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const app=express();
+const port=8080;
+const path=require("path");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
+})
+app.get("/ig/:username",(req,res)=>{
+    const followers=["Adam","bob","Dany","Smith"];
+    let {username}=req.params;
+    res.render("instagram.ejs",{username,followers});
+})
+app.get("/rolldice",(req,res)=>{
+    let num=Math.floor(Math.random()*6)+1;
+    res.render("rolldice.ejs",{diceval:num});
+})
+
+app.listen(port,()=>{
+    console.log(`listneing on the port ${port}`);
+})

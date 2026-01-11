d0256a8d9e98d74fb061222082f69195efbda5fb
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-01-11 15:42:52 +0530

    added redirect functionality

--- REST-dir/index.js
+++ REST-dir/index.js
@@ -29,7 +29,7 @@ app.get("/posts/new",(req,res)=>{
 app.post("/posts",(req,res)=>{
     let{username,content}=req.body;
     posts.push({username, content});
-    res.send("Post request is working!")
+    res.redirect("/posts"); 
 
 })
 

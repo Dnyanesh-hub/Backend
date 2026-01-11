6e91fce74bc8b0662483f45f67c9b80e0e4ff2a8
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-01-11 19:08:50 +0530

    Add edit route for posts

--- REST-dir/index.js
+++ REST-dir/index.js
@@ -3,8 +3,10 @@ const app=express();
 const port=8080;
 const path=require("path");
 const{v4: uuidv4}=require('uuid');
+const methodOverride=require("method-override");
 
 app.use(express.urlencoded({extended:true}))// to parse the data
+app.use(methodOverride("_method"));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")) );
@@ -37,13 +39,32 @@ app.post("/posts",(req,res)=>{
     posts.push({id,username, content});
     res.redirect("/posts"); 
 })
-app.get("/posts/:id",(req,res)=>{
-    let{id}=req.params;
-    let post=posts.find((p)=>id === p.id);
-    res.render("show.ejs",{post});
 
+app.get('/posts/:id',(req,res)=>{       //Get post by id
+    let {id}=req.params;
+    // console.log(id);
+    let post=posts.find((p)=>id===p.id);        //Find post using id
+    // console.log(post);
+    // res.send('server working well');
+    res.render('show.ejs',{post});
 })
+
+app.patch('/posts/:id',(req,res)=>{       //Update content of post
+    let {id}=req.params;
+    let newContent=req.body.content;
+    let post=posts.find((p)=>id===p.id);        //Find post using id
+    post.content=newContent;
+    console.log(post);
+    res.redirect('/posts');
+})
+
+app.get("/posts/:id/edit",(req,res)=>{          //Edit post
+    let {id}=req.params;
+    let post=posts.find((p)=>id===p.id);
+    res.render("edit.ejs",{post})
+})
+
 
 app.listen(port,(res,req)=>{
     console.log(`listening to port:${port}`);
-})
+}) 

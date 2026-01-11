a90111114d43f04cfff14d48b24792ce76e5174b
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-01-11 12:20:19 +0530

    created index route and added basic style for the page

--- REST-dir/index.js
+++ REST-dir/index.js
@@ -5,9 +5,23 @@ const path=require("path");
 app.use(express.urlencoded({extended:true}))// to parse the data
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
-app.set(express.static(path.join(__dirname,"public")) );
-app.get("/",(req,res)=>{
-    res.send("server working well!");
+app.use(express.static(path.join(__dirname,"public")) );
+let posts=[
+    {
+        username:"Dnyaeshwar Mali",
+        content:"I am the best",
+    },
+    {
+        username:"Dany",
+        content:"never give up!",
+    },
+    {
+        username:"Dnyaeshwar Mali",
+        content:"Disipline equals freedom-):",
+    }
+]
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
 })
 app.listen(port,(res,req)=>{
     console.log(`listening to port:  ${port}`);

5698bc31d1dd90bb0ddfb99c3e6b0070ebd313ba
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-01-15 18:12:55 +0530

    add show user and user count route

--- SQL/index.js
+++ SQL/index.js
@@ -1,12 +1,74 @@
 const { faker } = require('@faker-js/faker');
+const mysql= require('mysql2');
+const express=require("express");
+const app=express();
+const path=require("path");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+const port=8080;
+const connection =mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  database:'dany_app',
+  password:'q4w6fd87@723114'
+});
+// inserting new data in bulk using faker
 let getRandomUser = () =>{
-  return {
-    id: faker.string.uuid(),
-    username: faker.internet.username(),
-    email: faker.internet.email(),
-    avatar: faker.image.avatar(),
-    password: faker.internet.password(),
- 
-  };
+  return [
+    faker.string.uuid(),
+    faker.internet.username(),
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+};
+//Home route
+app.get("/",(req,res)=>{
+  let q=` SELECT count(*) FROM user`;
+  try{
+  connection.query(q,(err,result)=>{
+  if(err)throw err
+  let count=result[0]["count(*)"];
+  // res.send("SUCCESS");  
+
+  res.render("home.ejs",{count});
+});
+} catch(err){
+  console.log(err);
+  res.send("SOME ERROR IN DATABASE");
 }
-console.log(getRandomUser());
+});
+//show route
+app.get("/users",(req,res)=>{
+  let q=`SELECT * FROM user`;
+  try{
+  connection.query(q,(err,users)=>{
+  if(err)throw err
+  // console.log(result);
+  // res.send(result);
+  // res.send("SUCCESS");  
+  // res.render("home.ejs",{count});
+  
+  res.render("showusers.ejs",{users});
+});
+} catch(err){
+  console.log(err);
+  res.send("SOME ERROR IN DATABASE");
+}
+
+
+});
+app.listen(port,(res,req)=>{
+  console.log(`server is listening to port ${port}`);
+})
+// try{
+//   connection.query(q,[data], (err,result)=>{
+//   if(err)throw err
+//   console.log(result);
+  
+// })
+// } catch(err){
+//   console.log(err);
+// }
+// connection.end();
+
+

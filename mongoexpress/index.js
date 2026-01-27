6bfd5486b9a80dc1f3af4acf4b3825c00a52856b
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-01-27 18:15:05 +0530

    Error Handling

--- mongoexpress/index.js
+++ mongoexpress/index.js
@@ -5,6 +5,7 @@ const mongoose=require("mongoose");
 const path=require("path");  
 const Chat=require("./models/chat.js");
 const methodOverride = require("method-override");
+const ExpressError=require("./ExpressError.js");
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public") ));
@@ -17,7 +18,7 @@ main().then(()=>{
 .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/chatme');
+  await mongoose.connect('mongodb://127.0.0.1:27017/fakechatme');
 }
 //index route 
 app.get("/chats", async (req,res)=>{
@@ -29,12 +30,14 @@ app.get("/chats", async (req,res)=>{
 
 //new route
 app.get("/chats/new",(req,res)=>{
+    // throw new ExpressError(404,"page not found");
     res.render("new.ejs");
 
 });
 // create route
 app.post("/chats",(req,res)=>{
-     let{from,to,message}=req.body;
+     try{
+        let{from,to,message}=req.body;
      let newChat=new Chat(
         {
             from:from,
@@ -50,7 +53,20 @@ app.post("/chats",(req,res)=>{
 
         });
         res.redirect("/chats");
+     } catch(err){
+        next(err);
+     }
      
+});
+
+//new show route
+app.get("/chats/:id",async (req,res,next)=>{
+       let {id}=req.params;
+       let chat=await Chat.findById(id);
+       if(!chat){
+            next( new ExpressError(404,"chat not found or deleted"));
+       }
+       res.render("edit.ejs",{chat});
 });
 
 //edit route
@@ -86,6 +102,14 @@ app.delete("/chats/:id",async (req,res)=>{
 app.get("/",(req,res)=>{
     res.send("root is working well !");
 });
+// error handling middleware
+app.use((err,req,res,next)=>{
+    let {status=500,message="Some Error Occured"}=err;
+    res.status(status).send(message);
+
+
+});
+
 app.listen(port,()=>{
     console.log(`server is listening on port ${port}`)
 

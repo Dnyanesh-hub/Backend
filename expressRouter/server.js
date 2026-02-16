df181f0b4d5f346f8f74d7d969ad3bf3f7754466
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-02-16 23:52:26 +0530

    send cookies and using cookie parser

--- expressRouter/server.js
+++ expressRouter/server.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cookieParser = require("cookie-parser");
+app.use(cookieParser("secretecode"));
 
+//sending cookies
+app.get("/getsignedcookie", (req, res) => {
+  res.cookie("made-in","India",{signed:true});
+  res.send("signed cookie sent");
+});
+app.get("/getcookies", (req, res) => {
+  res.cookie("greet", "namste");
+  res.cookie("origin", "india");
+  res.send("Cookie sent successfully");
+});
+app.get("/greet", (req, res) => {
+  let { name = "anonymous" } = req.cookies;
+  res.send(`Hi,${name}`);
+});
 app.get("/", (req, res) => {
+  console.dir(req.cookies);
   res.send("Hi i am Root server Working very well");
 });
 
+//cookie parser pacakge
 //users
 //index route users
 app.get("/users", (req, res) => {
